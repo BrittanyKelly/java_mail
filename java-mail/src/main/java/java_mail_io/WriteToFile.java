f22811a20f767131df8e9a75b0444c3c62babf4c
Brittany Kelly <brittany.a.kelly@gmail.com>
2017-05-16 23:43:31 -0400

    Testing WriteToFile class

--- java-mail/src/main/java/java_mail_io/WriteToFile.java
+++ java-mail/src/main/java/java_mail_io/WriteToFile.java
@@ -1,8 +1,14 @@
 package java_mail_io;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
+import javax.mail.BodyPart;
 import javax.mail.Message;
+import javax.mail.internet.MimeMultipart;
 
 /**
  * This class facilitates the output of Message objects into individual files.
@@ -12,11 +18,77 @@ import javax.mail.Message;
  */
 public class WriteToFile {
     
-    private ArrayList<Message> messages;
-    
-    public WriteToFile(ArrayList<Message> msgs) {
+    public WriteToFile() {
         
     } 
+    
+    @SuppressWarnings("deprecation")
+    public void writeMessagesToFile(Message[] msgs) {
+        System.out.println("2" );
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        String filename = "";
+        try {
+            System.out.println("3" );
+            for (int i = 0; i <= msgs.length - 1; i++) {
+                System.out.println("4" );
+                filename = Integer.toString(msgs[i].getReceivedDate().getMonth()) +
+                        Integer.toString(msgs[i].getReceivedDate().getDay()) ;
+                File file = new File("C:\\Users\\Brittany\\Documents\\Receipts\\Receipts_" + filename + ".txt");
+                fw = new FileWriter(file, false);
+                bw = new BufferedWriter(fw);
+                
+                bw.write("---------------------------------");
+                bw.newLine();
+                bw.write("MESSAGE " + i + 1);
+                bw.newLine();
+                bw.write("DATE: " + msgs[i].getReceivedDate().toString());
+                bw.write("SUBJECT: " + msgs[i].getSubject());
+                bw.newLine();
+                bw.write("FROM: " + msgs[i].getFrom());
+                bw.newLine();
+                bw.write("BODY:");
+                bw.newLine();
+                bw.write(getTextFromMessage(msgs[i]));
+                bw.newLine();
+                bw.flush();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static String getTextFromMessage(Message message) throws Exception {
+        String result = "";
+        if (message.isMimeType("text/plain")) {
+            result = message.getContent().toString();
+        } else if (message.isMimeType("multipart/*")) {
+            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+            result = getTextFromMimeMultipart(mimeMultipart);
+        }
+        return result;
+    }
+
+    public static String getTextFromMimeMultipart(
+            MimeMultipart mimeMultipart) throws Exception{
+        String result = "";
+        int count = mimeMultipart.getCount();
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                result = result + "\n" + bodyPart.getContent();
+                break;
+            } else if (bodyPart.isMimeType("text/html")) {
+                String html = (String) bodyPart.getContent();
+                result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
+            } else if (bodyPart.getContent() instanceof MimeMultipart){
+                result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+            }
+        }
+        return result;
+    }
 
 }
  
--- java-mail/src/main/java/java_mail_main/IMAPMailSearch.java
+++ java-mail/src/main/java/java_mail_main/IMAPMailSearch.java
@@ -21,6 +21,8 @@ import javax.mail.search.SearchTerm;
 import com.sun.mail.gimap.GmailFolder;
 import com.sun.mail.gimap.GmailStore;
 
+import java_mail_io.WriteToFile;
+
 /**
      * Reference:
      * gimap javadoc https://javamail.java.net/nonav/docs/api/com/sun/mail/gimap/package-summary.html
@@ -54,22 +56,28 @@ import com.sun.mail.gimap.GmailStore;
                 GmailFolder receipts = (GmailFolder) store.getFolder("Receipts");
                 inbox.open(Folder.READ_ONLY);
                 receipts.open(Folder.READ_ONLY);
-                Message[] foundMessages = inbox.search(received);
+//                Message[] foundMessages = inbox.search(received);
+                Message[] foundMessages = receipts.getMessages();
+                System.out.print(foundMessages.length);
                 
-
-                ArrayList<Message> msgs = new ArrayList<Message>(foundMessages.length);
-                for (int i = foundMessages.length - 1; i >= 0; i--) {
-                   Message message = foundMessages[i];
-                   if (message.getSubject().toLowerCase().contains("receipt") ||
-                           getTextFromMessage(message).toLowerCase().contains("receipt") ||
-                           getTextFromMessage(message).toLowerCase().contains("$")){
-                       msgs.add(foundMessages[i]);
-                       
-                   }
-                   
-
-                }
-                writeMessagesToFile(msgs);
+    
+//                ArrayList<Message> msgs = new ArrayList<Message>(foundMessages.length);
+//                for (int i = foundMessages.length - 1; i >= 0; i--) {
+//                   Message message = foundMessages[i];
+//                   if (message.getSubject().toLowerCase().contains("receipt") ||
+//                           getTextFromMessage(message).toLowerCase().contains("receipt") ||
+//                           getTextFromMessage(message).toLowerCase().contains("$")){
+//                       msgs.add(foundMessages[i]);
+//                       
+//                   }
+//                   
+//
+//                }
+                
+                WriteToFile writer = new WriteToFile();
+                writer.writeMessagesToFile(foundMessages);
+                
+//                writeMessagesToFile(msgs);
                 
                 receipts.close(false);
                 inbox.close(false);
@@ -85,70 +93,70 @@ import com.sun.mail.gimap.GmailStore;
             System.out.println("Done");
             
         }
-        public static String getTextFromMessage(Message message) throws Exception {
-            String result = "";
-            if (message.isMimeType("text/plain")) {
-                result = message.getContent().toString();
-            } else if (message.isMimeType("multipart/*")) {
-                MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
-                result = getTextFromMimeMultipart(mimeMultipart);
-            }
-            return result;
-        }
-
-        public static String getTextFromMimeMultipart(
-                MimeMultipart mimeMultipart) throws Exception{
-            String result = "";
-            int count = mimeMultipart.getCount();
-            for (int i = 0; i < count; i++) {
-                BodyPart bodyPart = mimeMultipart.getBodyPart(i);
-                if (bodyPart.isMimeType("text/plain")) {
-                    result = result + "\n" + bodyPart.getContent();
-                    break;
-                } else if (bodyPart.isMimeType("text/html")) {
-                    String html = (String) bodyPart.getContent();
-                    result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
-                } else if (bodyPart.getContent() instanceof MimeMultipart){
-                    result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
-                }
-            }
-            return result;
-        }
-        
-        @SuppressWarnings("deprecation")
-        public static void writeMessagesToFile(ArrayList<Message> msgs) {
-            FileWriter fw = null;
-            BufferedWriter bw = null;
-            String filename = "";
-            try {
-                for (int i = 0; i < msgs.size() - 1; i++) {
-
-                    filename = Integer.toString(msgs.get(i).getReceivedDate().getMonth()) +
-                            Integer.toString(msgs.get(i).getReceivedDate().getDay()) ;
-                    File file = new File("C:\\Users\\Brittany\\Documents\\Receipts\\Receipts_" + i + ".txt");
-                    fw = new FileWriter(file, false);
-                    bw = new BufferedWriter(fw);
-                    
-                    bw.write("---------------------------------");
-                    bw.newLine();
-                    bw.write("MESSAGE " + i + 1);
-                    bw.newLine();
-                    bw.write("DATE: " + msgs.get(i).getReceivedDate().toString());
-                    bw.write("SUBJECT: " + msgs.get(i).getSubject());
-                    bw.newLine();
-                    bw.write("FROM: " + msgs.get(i).getFrom());
-                    bw.newLine();
-                    bw.write("BODY:");
-                    bw.newLine();
-                    bw.write(getTextFromMessage(msgs.get(i)));
-                    bw.newLine();
-                    bw.flush();
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+//        public static String getTextFromMessage(Message message) throws Exception {
+//            String result = "";
+//            if (message.isMimeType("text/plain")) {
+//                result = message.getContent().toString();
+//            } else if (message.isMimeType("multipart/*")) {
+//                MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+//                result = getTextFromMimeMultipart(mimeMultipart);
+//            }
+//            return result;
+//        }
+//
+//        public static String getTextFromMimeMultipart(
+//                MimeMultipart mimeMultipart) throws Exception{
+//            String result = "";
+//            int count = mimeMultipart.getCount();
+//            for (int i = 0; i < count; i++) {
+//                BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+//                if (bodyPart.isMimeType("text/plain")) {
+//                    result = result + "\n" + bodyPart.getContent();
+//                    break;
+//                } else if (bodyPart.isMimeType("text/html")) {
+//                    String html = (String) bodyPart.getContent();
+//                    result = result + "\n" + org.jsoup.Jsoup.parse(html).text();
+//                } else if (bodyPart.getContent() instanceof MimeMultipart){
+//                    result = result + getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+//                }
+//            }
+//            return result;
+//        }
+//        
+//        @SuppressWarnings("deprecation")
+//        public static void writeMessagesToFile(ArrayList<Message> msgs) {
+//            FileWriter fw = null;
+//            BufferedWriter bw = null;
+//            String filename = "";
+//            try {
+//                for (int i = 0; i < msgs.size() - 1; i++) {
+//
+//                    filename = Integer.toString(msgs.get(i).getReceivedDate().getMonth()) +
+//                            Integer.toString(msgs.get(i).getReceivedDate().getDay()) ;
+//                    File file = new File("C:\\Users\\Brittany\\Documents\\Receipts\\Receipts_" + i + ".txt");
+//                    fw = new FileWriter(file, false);
+//                    bw = new BufferedWriter(fw);
+//                    
+//                    bw.write("---------------------------------");
+//                    bw.newLine();
+//                    bw.write("MESSAGE " + i + 1);
+//                    bw.newLine();
+//                    bw.write("DATE: " + msgs.get(i).getReceivedDate().toString());
+//                    bw.write("SUBJECT: " + msgs.get(i).getSubject());
+//                    bw.newLine();
+//                    bw.write("FROM: " + msgs.get(i).getFrom());
+//                    bw.newLine();
+//                    bw.write("BODY:");
+//                    bw.newLine();
+//                    bw.write(getTextFromMessage(msgs.get(i)));
+//                    bw.newLine();
+//                    bw.flush();
+//                }
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
